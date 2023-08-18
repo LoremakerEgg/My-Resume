595a1a71449b453e45b96079e3c4f88efa3c7d5f
Anton <antonkoller@outlook.com>
2023-08-18 11:03:48 +0200

    started working on block menu styling - WIP

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -3,13 +3,34 @@
 import Link from "next/link";
 import styles from "./header.module.scss";
 import { usePathname } from "next/navigation";
+import { useState } from "react";
 
 const Header = () => {
   const pathName = usePathname();
+  const [sideMenuVisible, setSideMenuVisible] = useState(false);
+
+  const openSideMenu = () => {
+    console.log(sideMenuVisible);
+    setSideMenuVisible(!sideMenuVisible);
+  };
+
   return (
     <header className={styles.header}>
+      <div
+        className={styles.sideMenu}
+        style={{
+          width: sideMenuVisible ? "250px !important" : "0px !important",
+        }}
+      >
+        <a href="#">Test Link</a>
+      </div>
       <div>
-        <img src="menu-icon.png" alt="hamburger menu" className={styles.menu} />
+        <img
+          src="menu-icon.png"
+          alt="hamburger menu"
+          className={styles.menu}
+          onClick={openSideMenu}
+        />
       </div>
       <div className={styles.title}>
         <Link href="/" className={pathName == "/" ? "myLinkActive" : "myLink"}>
