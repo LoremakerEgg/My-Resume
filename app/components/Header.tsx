caebf0954b4263adf5ea0c234dc8345d3b1aaa9c
Anton <antonkoller@outlook.com>
2023-11-15 17:15:50 +0100

    fixed deployment issues

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -2,9 +2,33 @@
 
 import Link from "next/link";
 import styles from "./header.module.scss";
+import { useEffect, useState } from "react";
+
+function useWindowSize() {
+  const [windowSize, setWindowSize] = useState({
+    width: 0,
+    height: 0,
+  });
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    }
+    window.addEventListener("resize", handleResize);
+
+    handleResize();
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+  return windowSize;
+}
 
 const Header = () => {
-  if (window.innerWidth >= 1900) {
+  const size = useWindowSize();
+  if (size.width > 1000) {
     return (
       <header className={styles.header}>
         <img
@@ -25,7 +49,7 @@ const Header = () => {
         </div>
       </header>
     );
-  } else if (window.innerWidth < 1000) {
+  } else if (size.width < 600) {
     return (
       <header className={styles.header}>
         <img
